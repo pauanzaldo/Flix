16b9b395422544c88df9cfc6139259beeeb32a89
Paulina Anzaldo <panzaldo@fb.com>
2019-06-26 10:18:00 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by panzaldo on 6/26/19.
+//  Copyright © 2019 panzaldo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by panzaldo on 6/26/19.
+//  Copyright © 2019 panzaldo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
